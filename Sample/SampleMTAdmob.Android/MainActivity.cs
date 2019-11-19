312a80f8b3188e69dcf8a7bd0c4a296aad13c2ba
marcojak81 <mtroncone17@gmail.com>
2019-11-19 11:04:49 +0000

    Added Admob init to AndroidManifest

--- Sample/SampleMTAdmob.Android/MainActivity.cs
+++ Sample/SampleMTAdmob.Android/MainActivity.cs
@@ -1,10 +1,7 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
+using Android.Gms.Ads;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace SampleMTAdmob.Droid
@@ -21,6 +18,7 @@ namespace SampleMTAdmob.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            MobileAds.Initialize(this, " ca­-app-pub-­3940256099942544~3347511713");
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
