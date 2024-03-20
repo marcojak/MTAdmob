3874efc17eace4915d72e60803c4a4789295ab7c
Marco Troncone <mtroncone17@gmail.com>
2024-03-20 21:40:54 +0100

    Removed old code as not updated anymore. The plugin is now based on a private reposity

--- MTAdmob/Extra/MTRewardedAdOptions.shared.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MarcTron.Plugin.Extra
-{
-    public class MTRewardedAdOptions
-    {
-        public string CustomData { get; set; }
-
-        public string UserId { get; set; }
-    }
-}
